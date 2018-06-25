0a352b50cf71b13e938f3686b015478afefa5417
Malikabhc <malika.bouhcine@gmail.com>
2018-06-25 15:27:44 +0200

    exercice2_3

---
+++ assets/js/script2.js
@@ -0,0 +1,8 @@
+// Création d'une fonction pour masquer le texte
+function hideText() {
+  document.getElementById('text').style.display = 'none';
+}
+// Création d'une fonction pour afficher le texte
+function displayText() {
+  document.getElementById('text').style.display = 'block';
+}
